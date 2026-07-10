387a34320e66a25b16c1cda475b1eae7a4bbf358
TheCoder10t <55015125+TheCoder10t@users.noreply.github.com>
2026-07-10 15:46:08 +0800

    Add files via upload

---
+++ kent.py
@@ -0,0 +1,17 @@
+
+
+grade = 60.0
+
+if(grade < 60.1):
+    if(grade >= 60.1):
+        if(grade >= 60.1 or grade == 60.1):
+            print("*")
+        else:
+            print("!")
+        print("passed!")
+    else:
+        print("invalid")
+else:
+    print("good")
+    
+    
